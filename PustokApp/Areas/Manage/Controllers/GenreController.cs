302aecb963e49cab005bddb6639c22a502dc5a0d
Leyla-spec <leylasa@code.edu.az>
2025-08-26 18:12:10 +0400

    Update file

---
+++ PustokApp/Areas/Manage/Controllers/GenreController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PustokApp.Data;
+using PustokApp.Models.BookSlider;
+
+namespace PustokApp.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class GenreController
+        (PustokDbContex pustokDbContex)
+        : Controller
+    {
+        public IActionResult Index()
+        {
+            var genres = pustokDbContex.Genre.ToList();
+            return View(genres);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Create(Genres genre)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            if (pustokDbContex.Genre.Any(g => g.Name.ToLower() == genre.Name.ToLower()))
+            {
+                ModelState.AddModelError("Name", "This genre already exists");
+                return View();
+            }
+            pustokDbContex.Genre.Add(genre);
+            pustokDbContex.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int? id)
+        {
+            var genre = pustokDbContex.Genre.Find(id);
+            if (id == null)
+                return NotFound();
+            if (genre is null)
+                return NotFound();
+            pustokDbContex.Genre.Remove(genre);
+            pustokDbContex.SaveChanges();
+            return Ok();
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            var genre = pustokDbContex.Genre.Find(id);
+            if (genre is null)
+                return NotFound();
+            return View(genre);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Genres genre)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            var existGenre = pustokDbContex.Genre.Find(genre.Id);
+            if (existGenre is null)
+                return NotFound();
+            if (!ModelState.IsValid)
+                return View(existGenre);
+            if (pustokDbContex.Genre.Any(g => g.Name.ToLower() == genre.Name.ToLower() && g.Id != genre.Id))
+            {
+                ModelState.AddModelError("Name", "This genre already exists");
+                return View(existGenre);
+            }
+            existGenre.Name = genre.Name;
+            pustokDbContex.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Detail(int? id)
+        {
+            var genre = pustokDbContex.Genre.Find(id);
+            if (id == null)
+                return NotFound();
+            if (genre is null)
+                return NotFound();
+            return PartialView("_DetailModal", genre);
+        }
+    }
+}
---
+++ PustokApp/Migrations/20250826111358_mig_1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PustokApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sliders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Btntext = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BtnLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    Order = table.Column<int>(type: "int", nullable: false),
+                    CratedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sliders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    MainImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HoverImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    OldPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DiscountPercentage = table.Column<int>(type: "int", nullable: false),
+                    IsFeatured = table.Column<bool>(type: "bit", nullable: false),
+                    IsNew = table.Column<bool>(type: "bit", nullable: false),
+                    InStock = table.Column<bool>(type: "bit", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CratedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_books_Genre_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookImages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookImages_books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookTags",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookTags", x => new { x.BookId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_BookTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookTags_books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookImages_BookId",
+                table: "BookImages",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_books_AuthorId",
+                table: "books",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_books_GenreId",
+                table: "books",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookTags_TagId",
+                table: "BookTags",
+                column: "TagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookImages");
+
+            migrationBuilder.DropTable(
+                name: "BookTags");
+
+            migrationBuilder.DropTable(
+                name: "Sliders");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
